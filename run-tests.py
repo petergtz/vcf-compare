5be3fd6523cdfe6cb5d8a19b732350bc8926f61b
Peter Goetz <p3rg2z@gmail.com>
2013-04-30 23:48:23 +0200

    Minor improvements to run_tests.py + add licence

--- run-tests.py
+++ run-tests.py
@@ -1,26 +1,43 @@
 #!/usr/bin/python
 
-from os import listdir, makedirs, getcwd, chdir, remove
+#!/usr/bin/perl -w
+
+#   Copyright 2013 Peter Goetz
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+
+from os import listdir, makedirs, getcwd, chdir, remove, path
 import os.path
-from os import path
 from sys import argv
 from subprocess import call
 from shutil import rmtree
-import sys
 
 
 TEST_CASES_DIR = "test-cases"
 DIFF_VIEWER = argv[1] if len(argv) > 1 else "meld"
-
+SCRIPT_NAME = "vcf-compare"
 
 def main():
     assert os.path.exists(TEST_CASES_DIR)
     num_errors = 0
+    num_tests = 0
     for test_case in listdir(TEST_CASES_DIR):
+        num_tests += 1
         success = run(test_case)
         if not success:  num_errors += 1
     
-    print "\nTests completed. ERRORS:", num_errors
+    print "\n" + str(num_tests) + " Tests completed. ERRORS:", num_errors
     
 
 def run(test_case):
@@ -29,11 +46,13 @@ def run(test_case):
     ensure_dir_exists_and_is_empty(test_run_data_dir)
     success = True
     with InDirectory(test_run_data_dir):
-        rc = call([path.join(getcwd(), "..", "..", "vcf_compare.pl"), 
-                   path.join("..", "..", TEST_CASES_DIR, test_case, "a.vcf"),
-                   path.join("..", "..", TEST_CASES_DIR, test_case, "b.vcf")],
-                  stdout=open("actual-output", "w"),
-                  stderr=open("stderr", "w"))
+        with open("actual-output", "w") as actual_output_file, \
+             open("stderr", "w") as stderr_file:
+            rc = call([path.join(getcwd(), "..", "..", SCRIPT_NAME),
+                       path.join("..", "..", TEST_CASES_DIR, test_case, "a.vcf"),
+                       path.join("..", "..", TEST_CASES_DIR, test_case, "b.vcf")],
+                      stdout= actual_output_file,
+                      stderr=stderr_file)
         if rc != 0:
             print error(test_case, "script returned error. RC = " + str(rc))
             success = False
